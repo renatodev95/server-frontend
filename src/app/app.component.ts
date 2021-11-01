40933fbec0de7da615028fe2820fbec0b7209b7f
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2021-11-01 18:51:32 -0300

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { catchError, map, startWith } from 'rxjs/operators';
@@ -13,7 +13,8 @@ import { ServerService } from './service/server.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
   appState$: Observable<AppState<CustomResponse>>;
