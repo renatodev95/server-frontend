cc7b6f067e2724d9eac3a9e30afdc96f6d89c425
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2021-11-01 17:32:00 -0300

    filtering servers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent implements OnInit {
         }),
         startWith({ dataState: DataState.LOADING_STATE }),
         catchError((error: string) => {
-          return of({ dataState: DataState.ERROR_STATE, error })
+          return of({ dataState: DataState.ERROR_STATE, error });
         })
       );
   }
@@ -50,8 +50,23 @@ export class AppComponent implements OnInit {
         startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
         catchError((error: string) => {
           this.filterSubject.next('');
-          return of({ dataState: DataState.ERROR_STATE, error })
+          return of({ dataState: DataState.ERROR_STATE, error });
         })
       );
   }
+
+  filterServers(status: Status): void {
+    this.appState$ = this.serverService.filter$(status, this.dataSubject.value)
+      .pipe(
+        map(response => {
+          return { dataState: DataState.LOADED_STATE, appData: response }
+        }),
+        startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error });
+        })
+      );
+  }
+
+  
 }
