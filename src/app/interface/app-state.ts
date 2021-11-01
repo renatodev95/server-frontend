aaba44f2c57fc0a8278f8365b41b9637517d71ee
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2021-11-01 16:19:53 -0300

    service injection

--- src/app/interface/app-state.ts
+++ src/app/interface/app-state.ts
@@ -1,7 +1,7 @@
-import { DataState } from '../enum/data-stage.enum';
+import { DataState } from "../enum/data-state.enum";
 
 export interface AppState<T> {
-    data: DataState;
+    dataState: DataState;
     appData?: T;
     error?: string;
 }
